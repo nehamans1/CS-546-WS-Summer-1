78fcc7308f31f6167c38f4714f95c52cfdcf93c2
Neha Mansinghka <nehamansinghka@Nehas-MacBook-Pro.local>
2017-09-21 19:09:04 -0400

    Updated fileData.js functions with if and throw for each returned value from fs functions

--- Labs/lab_3/cs-546-lab-3/fileData.js
+++ Labs/lab_3/cs-546-lab-3/fileData.js
@@ -2,32 +2,72 @@
 //CWID: 10420458
 //Subject: CS546 Web Programming W1 Lab3
 
-//const bluebird = require("bluebird");
-//const Promise = bluebird.Promise;
 
-//const fs = bluebird.promisifyAll(require("fs"));
-const fs = require("fs");
+const bluebird = require("bluebird");
+
+const fs = bluebird.promisifyAll(require("fs"));
 
 module.exports = {
 
-    getFileAsString: async function (path, callback) {
+    getFileAsString: async function (path) {
 
-        fs.readFile(path, "utf-8", (error, data) => {
-            callback(error, data);
-          });
-
-       /* if ((!path)||(typeof(path)!=="string")){
+       if ((!path)||(typeof(path)!=="string")){
             throw "No filename provided";
-        }*/
-        /*
-        try{
-            const fileAsString = await fs.readFileAsync(path, "utf-8");
         }
-        catch(err){
-            console.log("Promise rejected");
-            console.log(err);
-        }       
-        callback(err, fileAsString);*/
+        
+        const fileAsString = await fs.readFileAsync(path, "utf-8");
+        if(fileAsString === "null"){
+            throw "Error in reading file";
+        }
+        return fileAsString;
+        
+    },
+
+    getFileAsJSON: async function (path){
+        
+        if ((!path)||(typeof(path)!=="string")){
+            throw "No filename provided";
+        }
+        
+        let fileContent = await fs.readFileAsync(path, "utf-8");
+        try{
+            return(JSON.parse(fileContent));
+        }
+        catch(parseErr){
+            console.log("Not a valid JSON format file");
+            return(parseErr);
+        }
+              
+    },
+        
+    saveStringToFile: async function (path, text){
+        if(typeof(path)=="undefined" || typeof(text)=="undefined"){
+            throw "Either the filename or the text is not provided.";
+        }
+
+        let fileObj = await fs.writeFileAsync(path, text, "utf-8");
+        if(fileObj === "null"){
+            throw "Error in writing file";
+        }
+        else{
+            return true;
+        }
+    
+    },
+        
+    saveJSONToFile: async function (path, obj){
+
+        if(typeof(path)=="undefined" || typeof(obj)=="undefined"){
+            throw "Either the filename or the object is not provided.";
+        }
+
+        let fileJSONObj = await fs.writeFileAsync(path, JSON.stringify(obj), "utf-8");
+        if(fileJSONObj === "null"){
+            throw "Error in writing JSON to file";
+        }
+        else{
+            return true;
+        }
     }
 }
 
